7ad968a45fd375d280b6557d2c718035e87f27a1
Said Elias Salcedo Cupitra <ssaidsal08@gmail.com>
2024-08-09 16:39:32 -0500

    Adding link into the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import c3po  from "../assets/JD-34-512.webp"
 import { useEffect, useState } from "react";
 import { HambugerMenu } from "./HamburgerMenu";
@@ -29,12 +29,11 @@ const Header = () => {
     }
   };
 
-  // const handleClick = () => {
-  //   if (openNavigation) {
-  //     setOpenNavigation(false);
+  const handleClick = () => {
+    // if (!openNavigation) return;
 
-  //   }
-  // };
+    setOpenNavigation(false);
+  };
 
   return (
     <div className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
@@ -51,17 +50,17 @@ const Header = () => {
           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:ml-auto lg:bg-transparent`}>
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.id}
-              href={item.url}
-              onClick={() => setOpenNavigation(false)}
+              to={item.url}
+              onClick={handleClick}
               className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === pathname.pathname ? "z-2 lg:text-n-1"
                   : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-color-1 xl:px-12`}
             >
               {item.title}
-            </a>
+            </Link>
             ))}
           </div>
             <HambugerMenu/>
